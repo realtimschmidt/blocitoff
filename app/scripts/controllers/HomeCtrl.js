e608157d00541f73835b15090b68afbd2f5dbcee
realtimschmidt <schmidt.tim.j@gmail.com>
2017-08-08 14:02:54 -0400

    Hide old tasks and complete tasks completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,19 +2,25 @@
   function HomeCtrl(Task) {
     this.tasks = Task.all;
 
+
+    this.hide = function(task) {
+     return task.created < (moment().dayOfYear() - 7) || task.completed == true
+    };
+
     this.addTask = function () {
       if (this.title) {
         this.task.$add({
           title: this.title,
           description: this.description,
-          createdAt: Date.now()
+          created: moment().dayOfYear(),
+          completed: this.completed
         });
         this.title = '';
       }
     };
 
-    this.expiredTask = function (createdAt) {
-      if (createdAt < Date.now() - 604800000) {
+    this.expiredTask = function (created) {
+      if (created < Date.now() - 604800000) {
         return true;
       }
     }
